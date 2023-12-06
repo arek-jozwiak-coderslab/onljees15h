ccf68d8ea6aa941c994edc1b08b36ed937aef0f2
arek jozwiak <arkadiusz.jozwiak@coderslab.pl>
2023-12-06 12:52:06 +0100

    messages

--- src/main/java/pl/coderslab/book/Book.java
+++ src/main/java/pl/coderslab/book/Book.java
@@ -13,7 +13,7 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Size(min = 5)
+    @Size(min = 3)
     private String title;
 
     @ManyToOne
